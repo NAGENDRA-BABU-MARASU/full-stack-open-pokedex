3f98b91b5e7556d44e651f814146af3a491c11c6
Nagendra <nagendrababu.marasu@gmail.com>
2024-03-03 11:44:40 +0530

    added health check endpoint

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 app.get('/version', (req, res) => {
-  res.send('1')
-})
+	res.send('1');
+});
+
+app.get('/health', (req, res) => {
+	res.send('ok');
+});
 
 app.listen(PORT, () => {
-  /* eslint-disable no-console */
-  console.log(`server started on port ${PORT}`)
-})
+	/* eslint-disable no-console */
+	console.log(`server started on port ${PORT}`);
+});
